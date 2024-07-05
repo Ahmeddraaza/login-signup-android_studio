ffcfc9fec58f8d9fcc852958a975592802904ded
Ahmed Raza <m.raza.25255@khi.iba.edu.pk>
2024-07-05 17:37:54 +0500

    dashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,12 +24,12 @@ android {
                 "proguard-rules.pro"
             )
         }
-        buildFeatures{
-           viewBinding = true
-        }
-
-
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,7 +37,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -45,7 +44,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation ("com.google.android.material:material:1.4.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
     implementation ("com.github.majorkik:SparkLineLayout:1.0.1")
-
 }
--- app/src/main/java/com/example/crypto_app/dashboard_activity.java
+++ app/src/main/java/com/example/crypto_app/dashboard_activity.java
@@ -1,15 +1,32 @@
 package com.example.crypto_app;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.crypto_app.apis.ApiUtilities;
+import com.example.crypto_app.apis.apiinterface;
+import com.example.crypto_app.marketmodel;
 
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class dashboard_activity extends AppCompatActivity {
+    private static final String TAG = "dashboard_activity";
     TextView profile_name, account_balancee;
     String email;
+    RecyclerView recyclerView;
+    cryptoadapter adapter;
+    ArrayList<CryptoModel> cryptoList = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +35,20 @@ public class dashboard_activity extends AppCompatActivity {
 
         profile_name = findViewById(R.id.textView9);
         account_balancee = findViewById(R.id.textView8);
+        recyclerView = findViewById(R.id.RecyclerViewnew);
 
         email = getIntent().getStringExtra("key_email");
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager); // Set layout manager
+        adapter = new cryptoadapter(this, cryptoList);
+        recyclerView.setAdapter(adapter);
+
         getuserdetails();
+        gettopcurrency();
     }
 
-    public void getuserdetails(){
+    public void getuserdetails() {
         database_model dbhelper = new database_model(this);
         ArrayList<userModal> al = dbhelper.getLoggedInuserdetail(email);
         if (!al.isEmpty()) {
@@ -33,4 +57,67 @@ public class dashboard_activity extends AppCompatActivity {
             account_balancee.setText(String.valueOf(userModal.getAccountbalance()));
         }
     }
+
+    public void gettopcurrency() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    apiinterface apiInterface = ApiUtilities.getInstance().create(apiinterface.class);
+                    Call<marketmodel> call = apiInterface.getMarketData();
+                    call.enqueue(new Callback<marketmodel>() {
+                        @Override
+                        public void onResponse(Call<marketmodel> call, Response<marketmodel> response) {
+                            if (response.isSuccessful()) {
+                                marketmodel marketModel = response.body();
+                                if (marketModel != null) {
+                                    CryptoDatabaseModel dbHelper = new CryptoDatabaseModel(dashboard_activity.this);
+                                    ArrayList<CryptoModel> tempList = new ArrayList<>();
+                                    for (marketmodel.CryptoCurrencyList data : marketModel.getData().getCryptoCurrencyList()) {
+                                        int id = data.getId();
+                                        String name = data.getName();
+                                        String symbol = data.getSymbol();
+                                        double price = data.getQuotes().get(0).getPrice();
+                                        double percentChange24h = data.getQuotes().get(0).getPercentChange24h();
+                                        double turnover = data.getQuotes().get(0).getTurnover();
+
+                                        // Check if the ID already exists in the database
+                                        if (!dbHelper.checkIfDataExists(id)) {
+                                            // Insert only if ID does not exist
+                                            dbHelper.insertCryptoData(id,name, symbol, price, percentChange24h, turnover);
+                                            CryptoModel cryptoModel = new CryptoModel(name, symbol, price, percentChange24h, turnover);
+                                            tempList.add(cryptoModel);
+                                        } else {
+                                            // Handle case where ID already exists (update or skip)
+                                            Log.d(TAG, "ID " + id + " already exists, skipping insertion or updating...");
+                                            CryptoModel cryptoModel = new CryptoModel(name, symbol, price, percentChange24h, turnover);
+                                            tempList.add(cryptoModel);
+                                        }
+                                    }
+
+                                    cryptoList.addAll(tempList);
+                                    adapter.notifyDataSetChanged();
+
+                                    System.out.println(cryptoList);
+
+                                    Log.d(TAG, "Data inserted into the database");
+                                }
+                            } else {
+                                Log.e(TAG, "Failed to get market data");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<marketmodel> call, Throwable t) {
+                            Log.e(TAG, "Exception in gettopcurrency", t);
+                        }
+                    });
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception in gettopcurrency", e);
+                }
+            }
+        }).start();
+    }
+
+
 }
